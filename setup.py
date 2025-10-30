88179e35ddc2b59e28ed03a93461cbb06c0e83be
santosh kumar <santosh.iitk7@gmail.com>
2025-10-31 01:51:39 +0530

    Requirement added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'AI Empowered Medical Chatbot',
+    version= '0.0.0',
+    author= 'Santosh Kumar',
+    author_email= 'santosh.iitk7@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
