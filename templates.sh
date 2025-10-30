0a7572734a0d64afb3094afa21757e4b58781f05
santosh kumar <santosh.iitk7@gmail.com>
2025-10-31 00:57:01 +0530

    file structured added

---
+++ templates.sh
@@ -0,0 +1,17 @@
+# Create the directory
+mkdir -p src
+mkdir -p research
+
+# Create the files
+touch src/__init__.py
+touch src/helper.py
+touch src/prompts.py
+touch .env
+touch requirements.txt
+touch setup.py
+touch app.py
+touch research/experiment.ipynb
+
+echo "Directories and files created successfully."
+
+
